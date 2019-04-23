fd2ba42de58ebb62ce41747c89bb53ba589f9545
zhouwenchao <844843839@qq.com>
2019-04-23 19:03:38 +0800

    提交一个项目

---
+++ src/test/java/demo1.java
@@ -0,0 +1,18 @@
+import cn.itcast.demo.QueueProducer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext-jms-producer.xml")
+public class demo1 {
+    @Autowired
+    private QueueProducer queueProducer;
+    @Test
+    public void testSend(){
+        queueProducer.sendTextMessage("SpringJms-点对点");
+    }
+}
